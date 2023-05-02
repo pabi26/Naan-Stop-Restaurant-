661ece8ab8e884d6960824410caefc6b8778ccbd
prabdeep thind <prabdeep_thind@hotmail.com>
2023-05-01 20:22:57 -0400

    styling issies

---
+++ menu.js
@@ -0,0 +1,53 @@
+const indianAppetizers = [
+    { name: 'Samosa', price: 7.99 }, 
+    { name: 'Samosa Channa: 2 PCS', price: 8.99 }, 
+    { name: 'Aloo Tikki', price: 6.99 },
+    { name: 'Bhaji', price: 9.99 },
+    { name: 'Paneer Tikka', price: 11.99 },
+    { name: 'Paneer Chilli Dry', price: 12.99 },
+    { name: 'Chicken Tikka', price: 12.99 },
+    { name: 'Tandoori Chicken', price: 13.99 },
+    { name: 'Seekh Kebab', price: 10.99 },
+    { name: 'Papadum', price: 5.99 },
+    { name: 'Chaat', price: 7.99 },
+    { name: 'Golgappe (Pani Puri)', price: 6.99 },
+    { name: 'Hakka Noodles', price: 10.99 },
+    { name: 'Hakka Chicken Noodles', price: 12.99 },
+    { name: 'Masala French Fries', price: 5.99 },
+    { name: 'Chilli Fish', price: 14.99 },
+    { name: 'Chilli Chicken', price: 13.99 },
+    { name: 'Chicken Pakora', price: 11.99 },
+  ];
+  
+  const indianMainVegDishes = [
+    { name: 'Aloo Gobi Masala', price: 12.99 },
+    { name: 'Veg Vindaloo', price: 14.99 },
+    { name: 'Mixed vegetables', price: 11.99 },
+    { name: 'Channa Masala', price: 12.99 },
+    { name: 'Yellow Dal Tadka', price: 11.99 },
+    { name: 'Dal Makhani', price: 13.99 },
+    { name: 'Bhindi Masala', price: 13.99 },
+    { name: 'Methi Malai Mutter', price: 14.99 },
+    { name: 'Shahi Paneer', price: 15.99 },
+    { name: 'Paneer Lababdar (Chef’s Special)', price: 16.99 },
+    { name: 'Paneer Tikka Masala', price: 15.99 },
+    { name: 'Palak/ Saag Paneer', price: 14.99 },
+    { name: 'Paneer Butter Masala', price: 15.99 },
+    { name: 'Achari Paneer', price: 15.99 },
+    { name: 'Mutter Paneer', price: 15.99 },
+  ];
+
+  const indianMainNonvegDishes = [
+    { name: 'Butter Chicken', price: 15.99 },
+    { name: 'Chicken Lababdar (Chef’s Special)', price: 16.99 },
+    { name: 'Kadhai Chicken', price: 14.99 },
+    { name: 'Chicken Korma', price: 14.99 },
+    { name: 'Chicken Curry', price: 13.99 },
+    { name: 'Lamb Curry', price: 15.99 },
+    { name: 'Lamb Kadhai', price: 16.99 },
+    { name: 'Goat Curry', price: 17.99 },
+  ];
+  
+
+  
+  
